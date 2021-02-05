1c237b323fe929880b4a3217c6f39e3c069b67b7
Pulkit Gupta <pulkitgupta@Pulkits-MacBook-Pro.local>
2021-02-05 17:30:48 +0530

    6th commit

--- src/main/java/com/thoughtworks/ddd/domain/Cart.java
+++ src/main/java/com/thoughtworks/ddd/domain/Cart.java
@@ -5,16 +5,19 @@ import java.util.List;
 
 public class Cart {
     private List<Item> items;
+    private List<Item> removedItems;
 
     public Cart() {
         this.items = new LinkedList<>();
+        this.removedItems = new LinkedList<>();
     }
 
     public void add(Item item) {
         this.items.add(item);
     }
 
-    public void remove(Item item){
+    public void remove(Item item) {
         this.items.remove(item);
+        this.removedItems.add(item);
     }
 }
