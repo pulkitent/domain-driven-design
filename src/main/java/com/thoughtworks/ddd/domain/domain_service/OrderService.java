7e32fd401523f3e1ac2fe94ae74578e10163a5d5
Pulkit Gupta <pulkitgupta@Pulkits-MacBook-Pro.local>
2021-02-06 13:04:44 +0530

    11th commit

---
+++ src/main/java/com/thoughtworks/ddd/domain/domain_service/OrderService.java
@@ -0,0 +1,9 @@
+package com.thoughtworks.ddd.domain.domain_service;
+
+import com.thoughtworks.ddd.domain.Order;
+
+public class OrderService {
+    public static Order placeOrder() {
+        return null;
+    }
+}
